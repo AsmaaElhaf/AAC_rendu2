82b389347debe33600f4c4f8e24efc60d5391ba3
asmaa el hafiane <asmaaelhafiane@MacBook-Pro-de-asmaa.local>
2024-02-25 23:38:05 +0100

    first commit

---
+++ Arbre de segments/build/CMakeFiles/min_avec_segments.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/min_avec_segments.dir/src/source.cpp.o"
+  "CMakeFiles/min_avec_segments.dir/src/source.cpp.o.d"
+  "libmin_avec_segments.a"
+  "libmin_avec_segments.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/min_avec_segments.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Arbre de segments/build/CMakeFiles/min_avec_segments.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for min_avec_segments.
+# This may be replaced when dependencies are built.
